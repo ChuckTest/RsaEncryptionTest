82656b76e76d67606a421fa824b9b538da9e4a93
Chuck LU <chucklu2015@outlook.com>
2021-12-14 17:25:41 +0800

    reference BigInteger for test

--- ChuckTest/TestClass.cs
+++ ChuckTest/TestClass.cs
@@ -1,18 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace ChuckTest
 {
+    //https://www.cnblogs.com/chucklu/p/5309297.html
+    //https://www.cnblogs.com/chucklu/p/5197231.html
     [TestFixture]
     public class TestClass
     {
+        [Test]
         public void Test2021_1214_001()
         {
+            //p=3, q=11
+            //n=p*q=33
+            //φ(n)=(p-1)*(q-1)=2*10=20
+            //e = 7, 1<e<φ(n)
+            //ed-1=kφ(n) => d=3 (ed-1=φ(n)) 3*7-1=1*20
+            //public key(e,n)=>(7,33)
+            //private key(d,n)=>(3,33)
+            var e = new BigInteger(7);
+            var d = new BigInteger(3);
+            var n = new BigInteger(33);
 
+            //encrypt m, then m^e = c (mod n) //=> m^7 = c (mod 33)
+            // (bnData ^ Exponent) % Modulus - This Encrypt the data using the public Exponent
+            var m = new BigInteger(2);
+            BigInteger c = BigInteger.ModPow(m,e, n);
+            Console.WriteLine(c);
+
+            //c^d = m (mod n), 也就是说，c的d次方除以n的余数为m。
+            BigInteger m1 = BigInteger.ModPow(c, d, n);
+            Console.WriteLine(m1);
         }
     }
 }
