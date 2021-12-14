ede281e3cee2b57b4a15c0d4be55212d6f46560e
Chuck LU <chucklu2015@outlook.com>
2021-12-14 16:57:23 +0800

    add a new test project

---
+++ ChuckTest/TestClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ChuckTest
+{
+    [TestFixture]
+    public class TestClass
+    {
+        public void Test2021_1214_001()
+        {
+
+        }
+    }
+}
